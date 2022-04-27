89891b7f3224389911630c734757df23e0f254fb
Ved misra <47312748+misraved@users.noreply.github.com>
2022-04-27 16:42:53 +0530

    Recompile plugin with steampipe-plugin-sdk-v3.1.0 and go v1.18 (#27)

--- equinix/table_equinix_metal_project_ssh_key.go
+++ equinix/table_equinix_metal_project_ssh_key.go
@@ -5,9 +5,9 @@ import (
 
 	metal "github.com/packethost/packngo"
 
-	"github.com/turbot/steampipe-plugin-sdk/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/plugin/transform"
+	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
 )
 
 func tableEquinixMetalProjectSSHKey(ctx context.Context) *plugin.Table {
