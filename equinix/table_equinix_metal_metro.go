d75b97980b98538ad4ff272f33bd0cf713a63db5
misraved <ved@turbot.com>
2022-09-07 03:14:51 +0530

    Recompile plugin with steampipe-plugin-sdk v4.1.6 and Go version 1.19

--- equinix/table_equinix_metal_metro.go
+++ equinix/table_equinix_metal_metro.go
@@ -5,8 +5,8 @@ import (
 
 	metal "github.com/packethost/packngo"
 
-	"github.com/turbot/steampipe-plugin-sdk/v3/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v4/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v4/plugin"
 )
 
 func tableEquinixMetalMetro(ctx context.Context) *plugin.Table {
